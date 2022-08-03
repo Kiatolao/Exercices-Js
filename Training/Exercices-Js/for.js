fe9b4197c186b85c7caea06f1fcab6dcc3592953
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2022-08-03 17:08:51 +0200

    Updated for exercices

---
+++ Training/Exercices-Js/for.js
@@ -0,0 +1,31 @@
+// exo - do 4 roud-abouts
+for (let t =0; t <4; t++) {
+    console.log (t);
+}
+console.log("----------------")
+
+// show numbers from 1 to 20 in console
+for (let i = 1; i <= 20; i++){
+    console.log(i);
+}
+console.log("----------------")
+
+// show number from 12 to 24 in console
+for (let n=12; n <= 24; n++) {
+    console.log(n);
+}
+console.log("----------------")
+
+// show numbers from 0 to 15 3 by 3.
+for (let x = 0; x <= 15; x += 3){
+    console.log(x);
+}
+console.log("----------------")
+
+// loop into loop
+for(let i= 1; i <= 3; i++) {
+    console.log(`${i} Going here`);
+    for(let n = 1; n <= 3; n++){
+        console.log(`${n} Going right there`)
+    }
+}
